5d2946eb24b5fd9eaf541257ab87142bca9961ae
Kristian Kanstrup <krjoh21@student.sdu.dk>
2024-04-04 14:57:31 +0200

    First  Commit

---
+++ src/test/java/dk/sdu/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.sdu.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

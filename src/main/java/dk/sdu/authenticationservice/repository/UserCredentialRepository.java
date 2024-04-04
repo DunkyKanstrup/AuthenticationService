095c6f10ed22d6e02fe52a985bd26d28df02c1b9
Kristian Kanstrup <krjoh21@student.sdu.dk>
2024-04-04 16:19:13 +0200

    Users

--- src/main/java/dk/sdu/authenticationservice/repository/UserCredentialRepository.java
+++ src/main/java/dk/sdu/authenticationservice/repository/UserCredentialRepository.java
@@ -3,5 +3,8 @@ package dk.sdu.authenticationservice.repository;
 import dk.sdu.authenticationservice.entity.UserCredential;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserCredentialRepository extends JpaRepository<UserCredential, Integer> {
+    Optional<UserCredential> findByName(String username);
 }

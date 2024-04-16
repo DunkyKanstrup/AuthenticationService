217bc25b28a3f6061f72645173edcbe96f4f0fea
Rasmus Helleberg Eriksen <raeri21@student.sdu.dk>
2024-04-16 14:37:09 +0200

    Added workflow

--- src/main/java/dk/sdu/authenticationservice/config/JwtService.java
+++ src/main/java/dk/sdu/authenticationservice/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,8 @@ import java.util.function.Function;
 public class JwtService {
 
     // Secret Key (64 Bytes)
-    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    @Value("${jwt.secret}")
+    private String secret;
 
     public String extractUserEmail(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -71,7 +73,7 @@ public class JwtService {
     }
 
     private Key getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 }
